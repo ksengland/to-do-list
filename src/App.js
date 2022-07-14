7f4f2194fa437ba3244d67af96db6dc7f96971d5
Karleen England <karleenengland@runbox.com>
2022-07-14 09:44:46 -0500

    completed task list

--- src/App.js
+++ src/App.js
@@ -4,30 +4,54 @@ import "./App.css";
 
 function App() {
   const [tasks, setTasks] = useState([
-    "walk dog",
-    "bake a cake",
-    "wash car",
-    "drink water",
-    "conquer the world",
+    { id: 1, description: "walk dog" },
+    { id: 2, description: "bake a cake" },
+    { id: 3, description: "wash car" },
+    { id: 4, description: "drink water" },
+    { id: 5, description: "conquer the world" },
   ]);
-  const [userTask, setUserTask] = useState("");
+  const [userTask, setUserTask] = useState({ id: 0, description: "" });
 
-  const handleClick = (event) => {
+  const handleSubmitTask = () => {
     setTasks([...tasks, userTask]);
-    setUserTask("");
+    setUserTask({ description: "" });
+  };
+
+  const handleChange = (event) => {
+    setUserTask({ id: tasks.length + 1, description: event.target.value });
+  };
+
+  const handleDelete = (clickedTask) => {
+    const filteredTasks = tasks.filter((task) => task.id !== clickedTask);
+    setTasks(filteredTasks);
+  };
+
+  const handleEdit = (id, editedTask) => {
+    const editedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        task.description = editedTask;
+        return task;
+      } else {
+        return task;
+      }
+    });
+
+    setTasks(editedTasks);
   };
   return (
     <div className="App">
       <h1>My Important Tasks!</h1>
-      <input
-        value={userTask}
-        onChange={(event) => setUserTask(event.target.value)}
-        type="text"
-      />
-      <button onClick={handleClick}>Add Task</button>
+      <input value={userTask.description} onChange={handleChange} type="text" />
+      <button onClick={handleSubmitTask}>Add Task</button>
 
       {tasks.map((task) => (
-        <Task description={task} key={task} />
+        <Task
+          id={task.id}
+          handleDelete={handleDelete}
+          description={task.description}
+          handleEdit={handleEdit}
+          key={task.id}
+        />
       ))}
     </div>
   );
@@ -35,5 +59,5 @@ function App() {
 
 export default App;
 
-//need to know if we have clicked on task itself or not
-//change style according to previous info
+//once user clicks on save edit we send information of task id + edited task to App
+//on App, iterate over tasks array, if task === edited task, return edited content instead of task itself
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,39 @@
 import { useState } from "react";
 
-const Task = ({ description }) => {
+const Task = ({ id, description, handleDelete, handleEdit }) => {
   const [isDone, setIsDone] = useState(false);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [editedTask, setIsEditedTask] = useState(description);
+
+  const handleSaveTask = () => {
+    setIsEditMode(false);
+    handleEdit(id, editedTask);
+  };
 
   return (
-    <div
-      onClick={() => {
-        return setIsDone(!isDone);
-      }}
-    >
-      <p className={isDone ? "task-description" : ""}>{description}</p>
+    <div className="task">
+      <div onClick={() => setIsDone(!isDone)}>
+        <p className={isDone ? "task-description" : ""}>
+          {isEditMode ? (
+            <input
+              onChange={(e) => setIsEditedTask(e.target.value)}
+              type="text"
+              value={editedTask}
+            />
+          ) : (
+            description
+          )}
+        </p>
+        {isEditMode && <button onClick={handleSaveTask}>Save</button>}
+      </div>
+      <div>
+        <button className="button" onClick={() => handleDelete(id)}>
+          X
+        </button>
+        <button className="button" onClick={() => setIsEditMode(!isEditMode)}>
+          ...
+        </button>
+      </div>
     </div>
   );
 };
